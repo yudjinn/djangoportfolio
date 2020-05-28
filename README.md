8171af9750152a32f5b1cf0f0d36ad93005863e1
yudjinn <jacob.rodgers94@gmail.com>
2020-05-27 21:11:11 -0600

    Rearranged and started adding functionality

---
+++ README.md
@@ -0,0 +1,1 @@
+To activate venv on windows: ./virt/Scripts/activate
---
+++ virt/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\my files\coding\portfolioapp\virt\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
