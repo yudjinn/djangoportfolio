7eddaaef15d0d52a1b730a1d793c313103d639ed
yudjinn <yudjinncoding@gmail.com>
2020-07-02 14:10:57 -0600

    Added login/logout functionality. Changed to class-based views

--- jcr/jcr/urls.py
+++ jcr/jcr/urls.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
 from resume import views as resume_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
     path('resume/', resume_views.resume, name='resume'),
-    path('', include('landing.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('', include('landing.urls'), name= 'landing'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
